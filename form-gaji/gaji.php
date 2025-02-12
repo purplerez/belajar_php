879a4dca1b0dd37c1a995e80eb80ef4d961d52b6
purplerez <fairy.edensor@gmail.com>
2025-02-12 12:49:16 +0700

    fix : tugas-gaji

--- form-gaji/gaji.php
+++ form-gaji/gaji.php
@@ -6,6 +6,36 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+/*
+Studi Kasus :
+Sebuah Perusahaan CV. TeknoNusa ingin membuat sistem penghitungan Gaji Bersih yang diberikan kepada karyawan setelah dipotong Pajak Penghasilan. Dengan Ketentuan sebagai berikut : 
+Karyawan akan menginputkan nama.
+Karyawan akan menginputkan Divisi dalam Perusahaan.
+Karyawan akan menginputkan total gaji tiap bulan.
+Karyawan akan memilih apakah dia memiliki NPWP atau tidak ( bisa menggunakan form select atau radio button )
+
+Sistem akan menghitung total gaji selama 1 tahun
+sistem akan menentukan apakah gaji orang tersebut terkena pajak atau tidak
+Jika total Gaji karyawan tersebut dalam 1 tahun lebih dari dari 54.000.000 , maka karyawan tersebut wajib membayar pajak,
+Jika total Gaji karyawan tersebut dalam 1 tahun kurang dari 54.000.000 maka karyawan tersebut tidak wajib membayar pajak
+
+untuk karyawan yang membayar pajak :
+Jika memiliki NPWP maka potongan pajak pertahunnya adalah total gaji * 15%
+Jika tidak memiliki NPWP maka potongan pajak pertahunnya adalah total gaji * 20%
+
+Tampilkan :
+Nominal Gaji Kotor Karyawan
+Nominal Potongan Pajak
+Nominal Gaji Perbulan yang diterima oleh karyawan tersebut ( baik yang wajib membayar pajak maupun tidak)
+
+*/
+
+
+
+
+?>
     
 
 
